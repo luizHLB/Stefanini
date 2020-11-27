e5014762ea43562f4a82f6d106814fde57cc917d
luiz.bortolloto <luiz.bortolloto@outlook.com>
2020-11-27 13:54:23 -0300

    MD5 encrypt

--- Stefanini.Business/Services/UserService.cs
+++ Stefanini.Business/Services/UserService.cs
@@ -4,7 +4,9 @@ using Stefanini.Domain.Entities;
 using Stefanini.Domain.Exceptions;
 using Stefanini.Domain.Interfaces.Repository;
 using Stefanini.Domain.Interfaces.Service;
-using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Stefanini.Business.Services
@@ -19,12 +21,20 @@ namespace Stefanini.Business.Services
 
         public async Task<User> Login(LoginModel login)
         {
+            login.Password = MD5Encrypt(login.Password);
+
             var user = await repository.GetUser(login);
             if (user is null)
                 throw new UserException("The email and/or password entered is invalid. Please try again.");
 
-
             return user;
+        }
+
+        private string MD5Encrypt(string password)
+        {
+            var md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(Encoding.ASCII.GetBytes(password));
+            return string.Join(string.Empty, md5.Hash.Select(s => s.ToString("x2")));
         }
     }
 }
